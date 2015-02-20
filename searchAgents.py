7e62bf6fd6638619b07e417f7cb5f75477f82653
myrmidon <dada3mn@gmail.com>
2015-02-20 20:07:19 +0100

    q5, FINALLY!!!!; everything before this was fucked up; this one works

--- searchAgents.py
+++ searchAgents.py
@@ -287,21 +287,22 @@ class CornersProblem(search.SearchProblem):
         self._expanded = 0 # DO NOT CHANGE; Number of search nodes expanded
         # Please add any code here which you would like to use
         # in initializing the problem
-        self.goal = set(self.corners)
-        self.visited = set()
+        self.startState = (self.startingPosition, 
+            tuple([self.startingPosition == corner for corner in self.corners]))
+        
 
     def getStartState(self):
         """
         Returns the start state (in your state space, not the full Pacman state
         space)
         """
-        return self.startingPosition
+        return self.startState
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
-        return self.goal <= self.visited.add(state)
+        return all(state[1])
 
     def getSuccessors(self, state):
         """
@@ -322,13 +323,16 @@ class CornersProblem(search.SearchProblem):
             #   dx, dy = Actions.directionToVector(action)
             #   nextx, nexty = int(x + dx), int(y + dy)
             #   hitsWall = self.walls[nextx][nexty]
-            x, y = state
+            x, y = state[0]
             dx, dy = Actions.directionToVector(action)
             nextx, nexty = int(x + dx), int(y + dy)
             if not self.walls[nextx][nexty]:
-                nextState = (nextx, nexty)
+                nextState = ((nextx, nexty), 
+                    tuple([(((nextx, nexty) == corner) or state_corner) 
+                        for corner, state_corner in zip(self.corners, state[1])]))
                 cost = 1
                 successors.append( (nextState, action, cost) )
+
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
